b10cad7afc05a9b9c3a85ac394035118af8daccf
Lutz Brandt <lutz.brandt@fairmas.com>
2016-11-23 18:56:58 +0100

    initial nonsense commit

---
+++ ContinuallyGuid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ContinuallyGuid
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating Guids ...");
+
+            while (true)
+            {
+                GuidClipboard.Program.Main(args);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ GuidClipboard/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuidClipboard
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var guid = Guid.NewGuid();
+            var guidString = guid.ToString();
+            Clipboard.SetText(guidString);
+        }
+    }
+}
